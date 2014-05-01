23e1b162fb3992443a7b938c319db546729fda89
Luca Mussi <ing.luca.mussi@gmail.com>
2014-05-01 11:24:21 +0200

    completed language support

--- client/views/nav.js
+++ client/views/nav.js
@@ -1,11 +1,17 @@
 Template.nav.rendered = function(){
     setActiveLinks();
-}
+};
 
 Template.nav.helpers({
     currLang: function(){
         return T9n.getLanguage().toUpperCase();
     },
+    languages: function(){
+        return T9n.getLanguages();
+    },
+    uppercase: function(){
+        return this.toUpperCase();
+    }
 });
 
 Template.nav.events({
