06141d699a7b5d65c0b47376e58064d24f6b0780
Luca Mussi <ing.luca.mussi@gmail.com>
2014-05-07 23:56:38 +0200

    added back accounts-merge plus developement social applications codes

--- shared/config/ATConfig.js
+++ shared/config/ATConfig.js
@@ -40,6 +40,7 @@ AccountsTemplates.configure({
     showPlaceholders: true,
     displayFormLabels: true,
     continuousValidation: true,
+    allowAccountsMerging: true,
 
     postSignUpRoutePath: '/profile',
     //signInRoutePath: '/signin',
